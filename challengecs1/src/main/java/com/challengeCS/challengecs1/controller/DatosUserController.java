cf910e7ad6ce0bb24fb7d9e487be9d88be2fa141
lashakita <gagino.melina@gmail.com>
2020-03-17 20:45:23 -0300

    se agrega docu

--- challengecs1/src/main/java/com/challengeCS/challengecs1/controller/DatosUserController.java
+++ challengecs1/src/main/java/com/challengeCS/challengecs1/controller/DatosUserController.java
@@ -1,16 +1,13 @@
 package com.challengeCS.challengecs1.controller;
 
 import com.challengeCS.challengecs1.model.DatosUser;
-import com.challengeCS.challengecs1.model.repository.UserRepository;
+import com.challengeCS.challengecs1.repository.UserRepository;
 import com.challengeCS.challengecs1.services.DatosUserSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 @RestController
 @RequestMapping ("/api")
--- challengecs1/src/main/java/com/challengeCS/challengecs1/repository/UserRepository.java
+++ challengecs1/src/main/java/com/challengeCS/challengecs1/repository/UserRepository.java
@@ -1,9 +1,7 @@
-package com.challengeCS.challengecs1.model.repository;
+package com.challengeCS.challengecs1.repository;
 
 import com.challengeCS.challengecs1.model.DatosUser;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
